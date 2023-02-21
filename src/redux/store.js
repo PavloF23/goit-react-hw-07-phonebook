b93758706c825cc2d9ae61b8df164831cf826fc1
PavloF23 <107143084+PavloF23@users.noreply.github.com>
2023-02-21 10:12:15 +0200

    up

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,9 +5,9 @@ import { filterReducer } from './filterSlice';
 export const store = configureStore({
 
   reducer: {
-    [contactReducer, reducerPath]: contactReducer.reducer,
+    [contact, reducerPath]: contactReducer.reducer,
     filter: filterReducer,
-    middleware: (getDefaultMiddleware) => [ ...getDefaultMiddleware(), contactReducer.middelware],
+    middleware: (getDefaultMiddleware) => [ ...getDefaultMiddleware(), contact.middelware],
 //  devTools: process.env.NODE_ENV !== 'production',
  devTools: process.env.NODE_ENV === 'development',
   },
