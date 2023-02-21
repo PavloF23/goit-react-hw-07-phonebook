573db0cd0629c6a410e6ae3b348cd5a76dc59896
PavloF23 <107143084+PavloF23@users.noreply.github.com>
2023-02-21 12:44:02 +0200

    up

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,7 +6,7 @@ import { Filter } from './Filter/Filter';
 import { Toaster } from 'react-hot-toast';
 import { getIsLoading, getError } from "redux/selectors";
 import { useSelector } from 'react-redux';
-import  Loader  from  '../components/Loader/Loader'
+import  Loader  from  './Loader/Loader'
 
 export function App() {
   const isLoading = useSelector(getIsLoading);
